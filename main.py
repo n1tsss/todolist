980ef6890b622df8a6ee002d33c0717ce744227a
243202 <243202@k733.edu>
2026-04-17 14:08:49 +0300

    create toto_list

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+import json
 def main():
     pass
 
 if __name__ == '__main__':
     main()
+
+def create_todo_list(path_todo, todo_name):
+    with open(path_todo, "w") as todo_file:
+        json.dump(
+            {"name": todo_name},
+            todo_file,
+            sort_keys=True,
+            indent=4,
+        )
